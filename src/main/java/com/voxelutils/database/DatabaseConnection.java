6b1ac8de5311c7de76b2f8bcbc6dfc619aa94b7c
Victor Guedes <victorfelipe.guedes@hotmail.com>
2020-12-11 20:21:59 -0300

    Adicionar conexão a banco de dados

---
+++ src/main/java/com/voxelutils/database/DatabaseConnection.java
@@ -0,0 +1,43 @@
+package com.voxelutils.database;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    String host, port, database, username, password;
+    Connection connection;
+    Statement statement;
+
+    public DatabaseConnection(String host, String port, String database,
+                              String username, String password) {
+        this.host = host;
+        this.port = port;
+        this.database = database;
+        this.username = username;
+        this.password = password;
+
+        connection = null;
+
+        try {
+            openConnection();
+            statement = connection.createStatement();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void openConnection() throws SQLException, ClassNotFoundException {
+        if (connection != null && !connection.isClosed()) {
+            return;
+        }
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        connection = DriverManager.getConnection("jdbc:mysql://"
+                + this.host + ":" + this.port + "/"
+                + this.database, this.username, this.password);
+    }
+
+    public Statement getStatement() {
+        return this.statement;
+    }
+}
